9e92f18f45fa0e9ccd2881c777995a7a706038e7
余玉林 <1240875925@qq.com>
2020-07-21 15:07:22 +0800

    提交

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hellove Cinema
+  </div>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" scoped></style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    hellove Mine
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'mine'
+}
+</script>
+<style lang="stylus" scoped></style>
